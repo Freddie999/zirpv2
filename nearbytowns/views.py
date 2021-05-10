8f5c04d563dcdae02543d87502f1f41227206122
Freddie <frederick.abalo@icloud.com>
2021-05-10 16:08:03 +0000

    fixed zoom level and nearby towns plotting on map

--- nearbytowns/views.py
+++ nearbytowns/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib.gis.geos import fromstr
-#from django.contrib.gis.db.models.functions import Distance
 from django.contrib.gis.measure import Distance
 from django.contrib.gis.geos import Point
 from .models import Town, Search
 from .forms import SearchForm
 import folium
+from folium import plugins
 import geocoder
+
 
 # Create your views here.
 def index(request):
@@ -28,22 +29,49 @@ def index(request):
     if lat == None or lng == None:
         address.delete()
         return HttpResponse('Your address/coordinates input is invalid')
+
     #create a map object
-    m = folium.Map(width=2000,height=740,location=[5.636114, -0.184089], zoom_start=12)
+    m = folium.Map(width=1450,height=700,location=[lat, lng], zoom_start=12, control_scale=True)
+
+    #add tiles to map
+    folium.raster_layers.TileLayer('Open Street Map').add_to(m)
+    #folium.raster_layers.TileLayer('Stamen Terrain').add_to(m)
+    folium.raster_layers.TileLayer('stamenterrain').add_to(m)
+    folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = False,
+        control = True).add_to(m)
+
+    #add layer control to show different maps
+    folium.LayerControl().add_to(m)
+
+
     folium.Marker([lat, lng]).add_to(m)
     folium.Marker([lat, lng], tooltip='Last known location',
                    popup=name).add_to(m)
 
     print(f"checking distance from {lat} {lng}")
-    town, town_loc = find_closest_town(lat, lng)
-    print(f"{town}")
-    print(f"{town_loc}")
-    # add a marker for the town
+   
+    #plot near towns to map
+    town_location_dict = find_closest_town(lat, lng)
+    for town in town_location_dict.items():
+        folium.CircleMarker(location=town[1], popup=town[0], tooltip='nearby town',
+        color='#dc143c', radius=2, weight=10
+        ).add_to(m)   
 
-    folium.CircleMarker([town_loc.y, town_loc.x], tooltip='Closest town to last known location',color='#dc143c', fill_color='#dc143c',
-         radius=6, weight=10,
-                   popup=name).add_to(m)
-    
+    #add measure control
+    measure_control = plugins.MeasureControl(position='topright',
+                                                    active_color='red',
+                                                    completed_color='red',
+                                                    primary_length_unit='kilometers'
+                                                    )
+    m.add_child(measure_control)
+
+    #add a draw tool
+    draw = plugins.Draw(export=True)
+    draw.add_to(m)
 
     #get html representation of map object
     m = m._repr_html_()
@@ -55,22 +83,22 @@ def index(request):
     #template_name = 'towns/index.html'
     return render(request, 'towns/index.html', context)
 
+
+
 def find_closest_town(lat, lng):
     all_towns = Town.objects.all()
     zip_location = Point(lng, lat, srid=4326)
     minimum_distance = 100000000
     closest_town = None
+    min_distance_towns_list = []
     for t in all_towns:
-        #distance = Distance(t.location, zip_location)
         distance = t.location.distance(zip_location)
         if distance < minimum_distance:
-            print(distance)
-            print("new minimum distance")
+            min_distance_towns_list.append((t.name,(t.location.y, t.location.x)))
             minimum_distance = distance
             closest_town = t
-    #print(closest_town.name)
-    #closest_town_loc = closest_town.location
-    #print(closest_town_loc)
-    
-    return closest_town.name, closest_town.location
+    new_min_dist_towns = min_distance_towns_list[-4:]
+    town_loc_dict = dict(new_min_dist_towns)
+    return town_loc_dict
+
 
